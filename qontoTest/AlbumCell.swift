edff3b71ea5dbfd29adacecb68a03ea5845e4d36
ypeysale <ypeysale@betomorrow.com>
2016-12-06 19:36:01 +0100

    Push AlbumViewController on click

---
+++ qontoTest/AlbumCell.swift
@@ -0,0 +1,14 @@
+//
+//  AlbumCell.swift
+//  qontoTest
+//
+//  Created by ypeysale on 06/12/2016.
+//  Copyright © 2016 ypeysale. All rights reserved.
+//
+
+import UIKit
+
+class AlbumCell: UITableViewCell {
+    @IBOutlet var albumNameLabel : UILabel!
+
+}
--- qontoTest/UsersTableViewController.swift
+++ qontoTest/UsersTableViewController.swift
@@ -45,15 +45,10 @@ class UsersTableViewController: UITableViewController {
         self.tableView.reloadData()
     }
 
-
-       /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "albumsSegue" {
+            (segue.destination as! AlbumsTableViewController)
+        }
     }
-    */
 
 }
